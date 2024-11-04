6c7e0f1e1bd71636efae8e47f9a22a67f9ea2360
Wesley Patterson <81829921+wezleyP@users.noreply.github.com>
2024-11-04 17:34:18 -0600

    Add project files.

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace wezGolf.Views.Inventory
+{
+    public class InventoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
